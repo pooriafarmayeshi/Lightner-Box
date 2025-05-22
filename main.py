50ad19060a5d529950fb9e75352e9495d6938b67
Pooria Farmayeshi Roodsari <165137647+pooriafarmayeshi@users.noreply.github.com>
2025-05-22 09:01:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-import tkinter
+import tkinter as tk
 
-root = tkinter.Tk ()
-root.geometry ( "1500x700" )
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("1000x600")
+        self.root.title("Litner Box")
+        self.center_window()
+        self.initialize_ui()
+    
+    def center_window(self):
+        # Opens the root at center
+        self.root.update_idletasks()
+        
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        
+        window_width = self.root.winfo_width()
+        window_height = self.root.winfo_height()
+        
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 2) - (window_height // 2) - 40
+        
+        self.root.geometry(f"+{x}+{y}")
+    
+    def initialize_ui(self):
+        # Initialize UI components
+        pass
+    
+    def run(self):
+        # Run the mainloop
+        self.root.mainloop()
 
-#Run the root
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.run()
